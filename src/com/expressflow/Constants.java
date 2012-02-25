449719109a1574068322aae127c86dedb3c2f4ad
Martin Vasko <martin@expressflow.com>
2012-02-25 18:13:38 +0100

    App Engine SDK update to latest

---
+++ src/com/expressflow/Constants.java
@@ -0,0 +1,19 @@
+package com.expressflow;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+/*
+	Copyright (c) 2011 Martin Vasko, Ph.D.
+
+	licensing@expressflow.com
+    http://expressflow.com/license
+*/
+
+public class Constants {
+	public static final String SESSION_USER = "&%$§&EXPRESS" ;
+
+	
+	public static String getHost(){
+			return "http://expresstasks.appspot.com/api/task/";
+	}
+}
---
+++ src/com/expressflow/security/KeyHolder.java
@@ -0,0 +1,33 @@
+package com.expressflow.security;
+
+public class KeyHolder {
+	
+	/*
+	 * TODO: Set the keys accordingly! Details can be found under:
+	 * 
+	 */
+	public static String EXPRESSTASKS_KEY = "--- Set accordingly ---";
+	public static String EXPRESSTASKS_SECRET = "--- Set accordingly ---";
+	
+	/*
+	 * TODO: Set the keys accordingly! Details can be found under:
+	 * https://www.dropbox.com/developers/app_info/902
+	 */
+	public static String DROPBOX_KEY = "--- Set accordingly ---";
+	public static String DROPBOX_SECRET = "--- Set accordingly ---";
+	
+	/*
+	 * TODO: Set the keys accordingly! Details can be found under:
+	 * http://code.google.com/intl/en-US/apis/gdata/docs/auth/oauth.html
+	 */
+	public static String GOOGLE_API_CLIENT_ID = "www.elasticflow.com";
+	public static String GOOGLE_API_CLIENT_SECRET = "--- Set accordingly ---";
+	public static String GOOGLE_API_K = "--- Set accordingly ---";
+	
+	/*
+	 * TODO: Set the keys accordingly! Details can be found under:
+	 * http://aws.amazon.com/security-credentials
+	 */
+	public static String AMAZON_ACCES_KEY = "--- Set accordingly ---";
+	public static String AMAZON_SECRET_KEY = "--- Set accordingly ---";
+}
